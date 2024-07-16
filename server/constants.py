d7fb16334a63555292ba569b31a8c861663920e6
Linoy7000 <131403675+Linoy7000@users.noreply.github.com>
2024-07-16 20:48:06 +0300

    Constants

---
+++ server/constants.py
@@ -0,0 +1,42 @@
+
+
+class SimulatorConstants:
+    SCALE = 4
+
+
+class ProductCategories:
+    PAROCHET = 1
+    SEFER_TORA = 2
+    BIMA = 2
+    OTHER = 4
+
+
+class OrderStatus:
+    DRAFT = 1
+    CLOSED = 2
+    PAID = 3
+
+
+class ContactSubject:
+    PAROCHET = 1
+    SEFER_TORA = 2
+    BIMA = 3
+    OTHER = 4
+
+
+class ContactStatus:
+    OPEN = 1
+    CLOSED = 2
+
+
+class TextPosition:
+    BOTTOM = 1
+    RIGHT = 2
+    LEFT = 3
+    CENTER = 4
+    TOP = 5
+
+class DIMENSIONS:
+    WIDTH = "1"
+    HEIGHT = "2"
+
